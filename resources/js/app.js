4034ab78197b27ad419b8be3b48eed8a9fc016da
maksanikienko <maksanikienko@gmail.com>
2024-02-05 13:31:04 +0200

    App In Progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
 import './bootstrap';
+import './aos';
+import './bootstrap';
+import './bootstrap.min';
+import './jquery';
+import './jquery.magnific-popup.min';
+import './main';
+import './owl.carousel.min';
+import './typed.min';
+// CSS
+
+//fonts and images
+import.meta.glob([
+    '../images/**',
+    '../fonts/**',
+]);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,32 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
 export default defineConfig({
+    build: {
+        sourcemap: true,
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/js/app.js',
+                'resources/js/jquery.js',
+                'resources/js/jquery.magnific-popup.min.js',
+                'resources/js/aos.js',
+                'resources/js/bootstrap.js',
+                'resources/js/bootstrap.min.js',
+                'resources/js/main.js',
+                'resources/js/owl.carousel.min.js',
+                'resources/js/typed.min.js',
+
+                'resources/css/app.css',
+                'resources/css/aos.css',
+                'resources/css/bootstrap.min.css',
+                'resources/css/icofont.min.css',
+                'resources/css/magnific-popup.min.css',
+                'resources/css/owl.carousel.min.css',
+                'resources/css/responsive.css',
+                'resources/css/style.css'
+
+            ],
             refresh: true,
         }),
     ],
